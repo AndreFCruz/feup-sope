470c3f33cab27d7e4b0de91af0d1cadca401eb99
bruno02960 <briguel1997>
2017-04-27 20:02:28 +0100

    gerador.c first commit

---
+++ Proj2/gerador.c
@@ -0,0 +1,80 @@
+struct request_t {
+	static int serial_no;
+	char gender;
+	int duration;
+	int no_rejections;
+}
+
+int request_t::serial_no=0;
+
+int max_requests=0;
+
+int max_duration=0;
+
+int in_fd, out_fd;
+
+
+/* YET TO FINISH 
+void * gen_request (void * arg)
+{
+	struct request_t req;
+		
+	int gen = rand() % 2;
+	if(gen==0)
+		req.gender='M';
+	else
+		req.gender='F';
+
+	int req.duration = rand() % max_duration;
+
+
+	void *ret=malloc(sizeof(struct request_t));
+	* (struct request_t*) ret=req;
+		
+	return ret;
+}*/
+
+int main(int argc, char argv[][]){
+	srand(time(NULL));
+
+	/* BEGIN Storing the main args */
+
+	max_requests=atoi(argv[1]);
+	max_duration=atoi(argv[2]);
+
+	if(strlen(argv[3]==1))
+		time_unit=argv[3][1];
+	else
+		exit(1);
+
+	/* END Storing the main args */
+
+	/* BEGIN Making and opening FIFOs */
+
+	if (mkfifo("/tmp/entrada",0660)<0)
+	{
+		if (errno==EEXIST)
+			printf("FIFO /tmp/entrada already exists\n");
+		else
+			printf("Can't create FIFO\n");
+
+		exit(2);
+	}
+
+	if ((in_fd=open("/tmp/entrada",O_WRONLY)) ==-1)
+		{
+			printf("Can't open FIFO /tmp/entrada\n");
+			exit(3);
+		}
+
+	if ((out_fd=open("/tmp/rejeitados",O_RDONLY)) ==-1)
+		{
+			printf("Can't open FIFO /tmp/rejeitados\n");
+			exit(4);
+		}
+
+	/* END Making and oppening FIFOs */
+
+
+	exit(0);
+}

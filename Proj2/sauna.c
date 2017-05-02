d1662fa62480824f859d1dfa17abaf6fb955155d
bruno02960 <briguel1997>
2017-05-02 21:46:55 +0100

    sauna refactoring

--- Proj2/sauna.c
+++ Proj2/sauna.c
@@ -6,52 +6,74 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <semaphore.h>
 #include <string.h>
+
+#define SHARED 0
 
 int no_places = 0;
 char gender;
 int out_fifo, in_fifo;
 int out_fd;
-char time_unit;
+int time_init;	/* Initial time */
 
-/*
-void writeRegist(int thread_id, struct request_t req)
-{
-	// Write formatted (time_instant ?) , pid, tid, req.serial_no, req.gender, req.duration, (tip ?)
-}
-*/
+sem_t out_sem;
+sem_t places_sem;
+
+pthread_mutex_t mut=PTHREAD_MUTEX_INITIALIZER;
 
 /**
 * Simulates the steam room utilisation
 */
-void * utilisation_sim(void *arg){
-	int time = * (int *) arg;
+void * utilisation_sim(void *arg);
 
-	sleep(time);
+/**
+* Making and opening FIFOs and regist file
+*/
+void fileHandler();
+
+int main(int argc, char** argv){
+	if(argc!=2)
+	{
+		printf("Usage: %s <no_places> \n", argv[0]);
+		exit(1);
+	}
+
+	//Storing the main args
+	no_places=atoi(argv[1]);
+
+	//Semaphore intializer
+	seminit(&out_sem, SHARED, 1);
+
+	fileHandler();
+
+	//Closing files and deleting created FIFO
+	close(in_fifo);
+	close(out_fifo);
+	unlink("/tmp/rejeitados");
+
+	exit(0);
+}
+
+void * utilisation_sim(void *arg){
+	struct request_t req = * (struct request_t *) arg;
+	pthread_t tid = pthread_self();
+	int time_instant=0;
+	char* tip = 'SERVED';
+
+	sleep(req.duration);
+	sem_wait(&out_sem);
+	dprintf(out_fd, "%-5d, %-5d, %-5d, %-5d, %-2c, %-5d, %-10s\n", time_instant, pid, tid, req.serial_no, req.gender, req.duration, /*(tip ?)*/);
+	sem_post(&out_sem);
+
+	pthread_mutex_lock(&places_sem); 
+	no_places--;
+ 	pthread_mutex_unlock(&places_sem); 
 
 	return NULL;
 }
 
-int main(int argc, char** argv){
-	if(argc!=3)
-	{
-		printf("Usage: %s <no_places> <time_unit>\n", argv[0]);
-		exit(1);
-	}
-
-	/* BEGIN Storing the main args */
-
-	no_places=atoi(argv[1]);
-
-	if(strlen(argv[2])==1)
-		time_unit=argv[2][1];
-	else
-		exit(1);
-
-	/* END Storing the main args */
-
-	/* BEGIN Making and opening FIFOs and regist_file */
-
+void fileHandler(){
 	if (mkfifo("/tmp/rejeitados",0660)<0)
 	{
 		if (errno==EEXIST)
@@ -75,8 +97,6 @@ int main(int argc, char** argv){
 		exit(4);
 	}
 
-	printf("HERE!\n");
-
 	int pid = getpid();
 	char * filename=malloc(sizeof(char)*50);
 	snprintf(filename, 50, "/tmp/bal.%d", pid);
@@ -86,8 +106,4 @@ int main(int argc, char** argv){
 		printf("Can't open FIFO %s\n",filename);
 		exit(6);
 	}
-
-	/* END Making and oppening FIFOs and regist_file */
-
-	exit(0);
 }
